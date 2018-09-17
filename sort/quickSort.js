65735f477d698fa036fa7730eac7c4e41662747d
i-wanrumeng <i-wanrumeng@360.cn>
2018-09-17 20:33:29 +0800

    update:增加原地快排

--- sort/quickSort.js
+++ sort/quickSort.js
@@ -5,29 +5,58 @@
 * @Last Modified time: 17:52
 */
 //复杂度为O(nlogn)
-function quickSort(arr) {
-    let length = arr.length;
-    let right = [],left = [];
-    //轴心值取第一个数
-    let temp = arr[0];
-    //递归终止条件
-    if (length <= 1){
-        return arr;
-    }
-    for(let i = 1;i<length;i++){
-        //大于轴心值的数放到右边数组中，小于的放在左边数组中
-        if (arr[i] < temp){
-            left.push(arr[i]);
-        }
-        else{
-            right.push(arr[i]);
-        }
-    }
-    //进行递归调用，并将左边数组与轴心值和右边数组链接起来
-    return quickSort(left).concat([temp],quickSort(right));
-}
+// 非原地快排（阮一峰版快排）
+// function quickSort(arr) {
+//     let length = arr.length;
+//     let right = [],left = [];
+//     //轴心值取第一个数
+//     let temp = arr[0];
+//     //递归终止条件
+//     if (length <= 1){
+//         return arr;
+//     }
+//     for(let i = 1;i<length;i++){
+//         //大于轴心值的数放到右边数组中，小于的放在左边数组中
+//         if (arr[i] < temp){
+//             left.push(arr[i]);
+//         }
+//         else{
+//             right.push(arr[i]);
+//         }
+//     }
+//     //进行递归调用，并将左边数组与轴心值和右边数组链接起来
+//     return quickSort(left).concat([temp],quickSort(right));
+// }
+//
+// console.log(quickSort([5,3,6,1,8]));
 
-console.log(quickSort([5,3,6,1,8]));
+// 原地快排
+function quickSort(arr,left,right) {
+    let len = arr.length;
+    let partitionIndex;
+    left = typeof left !== 'number' ? 0 : left;
+    right = typeof right !== 'number' ? len - 1 : right;
+    if (left < right){
+        partitionIndex = partition(arr,left,right);
+        quickSort(arr,left,partitionIndex - 1);
+        quickSort(arr,partitionIndex + 1,right);
+    }
+    return arr
+}
+function partition(arr,left,right) {
+    let pivot = left;
+    let index = pivot + 1;
+    for (let i = index;i <= right;i++){
+        console.log(arr)
+        if (arr[i] < arr[pivot]){
+            [arr[index],arr[i]] = [arr[i],arr[index]];
+            index ++;
+        }
+    }
+    [arr[pivot],arr[index-1]] = [arr[index -1],arr[pivot]];
+    return index - 1;
+}
+console.log(quickSort([5,3,6,1,2,8]));
 
 //递归思想
 //1.有终止条件
